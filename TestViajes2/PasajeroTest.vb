1b542d8aee2268af50469e5d28eaaeac806a67b8
rominad <37489199+rominad@users.noreply.github.com>
2018-11-26 18:10:13 -0300

    second commit

---
+++ TestViajes2/PasajeroTest.vb
@@ -0,0 +1,9 @@
+﻿Imports Viajes2
+Module PasajeroTest
+    Sub Main()
+        Dim pasajero1 As New Pasajero(38123456, "NombrePasajero", "DomicilioPasajero", #5/31/1994#)
+        Console.WriteLine(pasajero1.ToString())
+        Console.WriteLine(pasajero1.isMayorEdad())
+        Console.ReadKey()
+    End Sub
+End Module
--- TestViajes2/VentasTest.vb
+++ TestViajes2/VentasTest.vb
@@ -1,7 +1,7 @@
 ﻿Imports Viajes2
 Module VentasTest
     Sub Main()
-        Dim pasajero1 As New Pasajero(38123456, "NombrePasajero1", "DomicilioPasajero", Date.Now(), DateDiff)
+        Dim pasajero1 As New Pasajero(38123456, "NombrePasajero1", "DomicilioPasajero", Date.Now())
         Dim pasajero2 As New Pasajero(38123756, "NombrePasajero2", "DomicilioPasajero", Date.Now())
         Dim cliente1 As New Cliente("0123456789123", "NombreCliente", "DireccionCliente")
         Dim venta1 As New Ventas(Date.Now(), cliente1)
@@ -12,8 +12,9 @@ Module VentasTest
             Console.Write("-", pasajero.Nombre)
         Next
         Console.WriteLine("Precio Pesos: {0}", venta1.PrecioPesos())
-        Console.WriteLine("Precio Dolar: {0}", venta1.PrecioDolar())
+        Console.WriteLine(venta1.PrecioDolar())
         Console.WriteLine("Precio Total: {0}", venta1.PrecioTotal())
         Console.ReadKey()
     End Sub
 End Module
+
